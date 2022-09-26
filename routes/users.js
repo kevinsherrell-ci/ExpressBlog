e458cca21c074f4cf72248c629f7ff5ee78b7c51
kevinsherrell <k.sherrell.1028@gmail.com>
2022-09-25 23:20:08 -0500

    validation complete

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const validateUserData = require('../validation/users.js');
+const userList = [];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
+router.post("/create-one", (req, res) => {
+    console.log("create user is running");
+    let userData = {
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        age: req.body.age,
+        favoriteFoods: req.body.favoriteFoods || [],
+        createdAt: Date.now(),
+        lastModified: Date.now(),
+        getFullName() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+    }
+
+    console.log(userData.getFullName());
+    const userDataCheck = validateUserData(userData);
+
+    if (userDataCheck.isValid === false) {
+        return res.json({
+            success: false,
+            errors: userDataCheck.errors
+        });
+    }
+
+    userList.push(userData);
+    console.log("userList", userList);
+    return res.json({
+        success: true
+    });
+})
 module.exports = router;
---
+++ validation/users.js
@@ -0,0 +1,76 @@
+const validateUserData = (userData) => {
+
+    const errors = [];
+    const isEmpty = data => data === undefined || data === null || (isString(data) && data.trim() === "") || (typeof data === 'object' && Object.keys(data).length === 0);
+    const isString = data => typeof data === "string";
+    const isArray = data => Array.isArray(data);
+    const isNumber = data => typeof data === 'number';
+
+    const addError = (type, message)=>{
+        errors.push({
+            type: type,
+            message: message
+        });
+    }
+    if (isEmpty(userData)) {
+
+        addError("userData", "userData cannot be empty");
+    }
+
+    // email checks
+    if(isEmpty(userData.email)){
+
+        addError("email", "email cannot be empty");
+    }
+    if (!isString(userData.email)) {
+        addError("email", "email must be a string");
+    }
+    // first name checks
+    if(isEmpty(userData.firstName)){
+        addError("firstName", "firstName cannot be empty");
+    }
+    if (!isString(userData.firstName)) {
+        addError("firstName", "firstname must be a string");
+    }
+    // last name checks
+    if(isEmpty(userData.lastName)){
+        addError("lastName", "last name cannot be empty");
+    }
+    if (!isString(userData.lastName)) {
+        addError("lastName", "lastname must be a string");
+    }
+    // age checks
+    if(isEmpty(userData.age)){
+        addError("age", 'age must not be empty');
+    }
+    if(!isNumber(userData.age)){
+        addError("age", "age must be a number");
+    }
+    // favorite food checks
+
+    if (!isArray(userData.favoriteFoods)) {
+        addError("favoriteFoods", "favorite foods must be an array");
+    }
+    if(userData.favoriteFoods.length > 0){
+        userData.favoriteFoods.forEach(food => {
+            if (isString(food)) {
+                addError("favoriteFoods", "favorite foods must be strings");
+            }
+        })
+    }
+
+
+
+    if(errors.length > 0){
+        return {
+            isValid: false,
+            errors: errors
+        }
+    }
+    return {
+        isValid: true
+    }
+
+}
+
+module.exports = validateUserData;
